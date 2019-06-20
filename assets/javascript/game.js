1f1799528dfab3c16b386d13ef6bdce65357e48e
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-06-20 01:08:37 -0700

    getting functions going

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,3 +2,8 @@ var wins = 0;
 var losses = 0;
 var target = 0;
 var totalpoints = 0;
+
+Function getTheNumber(result) {
+    var randomNumber = Math.floor((Math.random() * 120) +19);
+    var randomNumberCrystal = Math.floor((Math.random() * 12) +11);
+} 
