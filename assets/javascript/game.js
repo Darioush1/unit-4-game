4cd26a1efdd9ecac5dfdb721e9eda304fedcd625
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-07-05 11:59:13 -0700

    value's labeled

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,41 @@ var losses = 0;
 var target = 0;
 var totalpoints = 0;
 
-Function getTheNumber(result) {
-    var randomNumber = Math.floor((Math.random() * 120) +19);
+$(document).ready(function() {
+
     var randomNumberCrystal = Math.floor((Math.random() * 12) +11);
-} 
+    var randomNumber = Math.floor((Math.random() * 120) +19); 
+    var crystalNumberButtons = {
+        black=
+        {
+            name= "black",
+            value= 0
+        },
+
+        blue=
+        {
+            name= "blue",
+            value= "0"
+        },
+
+        green= 
+        {
+            name= "green",
+            value= "0",
+        },
+
+        purple=
+        {
+            name= "purple",
+            value= "0"
+        }
+    }
+
+
+    })
+}); 
+
+console.log(randomNumberCrystal);
+$(document).on('click', function{
+
+})
