5520dc6270e12b2bd120a1b5d827948050b47e6d
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-07-24 22:34:11 -0700

    after help from Mac

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,92 @@
 var wins = 0;
 var losses = 0;
-var target = 0;
 var totalpoints = 0;
+var randomNumber; 
 
-$(document).ready(function() {
+    
 
-    var randomNumberCrystal = Math.floor((Math.random() * 12) +11);
-    var randomNumber = Math.floor((Math.random() * 120) +19); 
     var crystalNumberButtons = {
-        black=
+        black:
         {
-            name= "black",
-            value= 0
+            name: "black",
+            value:  Math.floor((Math.random() * 12)+1)
         },
 
-        blue=
+        blue:
         {
-            name= "blue",
-            value= "0"
+            name: "blue",
+            value:  Math.floor((Math.random() * 12)+1)
         },
 
-        green= 
+        green:
         {
-            name= "green",
-            value= "0",
+            name: "green",
+            value:  Math.floor((Math.random() * 12)+1),
         },
 
-        purple=
+        purple:
         {
-            name= "purple",
-            value= "0"
+            name: "purple",
+            value:  Math.floor((Math.random() * 12)+1)
         }
-    }
+    };
+
+    $('#wins').append(wins);
+    $('#losses').append(losses);
+    $('#totalpoints').append(totalpoints);
+
+function runGame() {
+    totalpoints = 0;
+    randomNumber = Math.floor((Math.random() * 120) +19);
+    $('#randomNumber').text(randomNumber);
+};
+
+ runGame();
+
+$('.blackCrystal').on('click', function(){
+    $('.blackCrystal').text(crystalNumberButtons.black.value);
+    $('#totalpoints').append(crystalNumberButtons.black.value);
+    updateTotalPoints();
+});
+$('.blueCrystal').on('click', function (){
+    $('.blueCrystal').text(crystalNumberButtons.blue.value);
+});
+$('.greenCrystal').on('click', function (){
+    $('.greenCrystal').text(crystalNumberButtons.green.value);
+});
+$('.purpleCrystal').on('click', function (){
+    $('.purpleCrystal').text(crystalNumberButtons.purple.value);
+});
+
+// get value of button, update the total score, append to page
 
 
-    })
-}); 
 
-console.log(randomNumberCrystal);
-$(document).on('click', function{
+function updateTotalPoints(crystalButtons) {
+    totalpoints += crystalNumberButtons[crystalNumberButtons.attr('.buttons')].points;
+    $('#totalpoints').append(totalpoints);
+};
 
-})
+
+
+
+console.log(crystalNumberButtons)
+
+
+
+console.log('crystalNumberButtons.black.value' + randomNumberCrystalBlack);
+console.log('Blue ' + randomNumberCrystalBlue);
+console.log('Green ' + randomNumberCrystalGreen);
+console.log('Purple ' + randomNumberCrystalPurple);
+console.log(randomNumber)
+console.log(updateTotalPoints)
+
+
+/*
+
+function resetGame() {
+    // set new random number
+}
+
+
+*/
